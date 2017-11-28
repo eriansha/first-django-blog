df3c4754736ac8349400ee3258d7436853b4e3fd
eriansha <putra.ivan90@gmail.com>
2017-11-28 20:14:28 +0700

    Add urls in firstproject

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    #url(r'^hello/', hello, name='hello'),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls'))
 ]
